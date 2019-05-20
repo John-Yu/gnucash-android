088fdb6adc0f2a44b9af28af648a825fe3308950
Jordy Quak <jordy.quak@hva.nl>
2019-05-20 14:37:09 +0200

    Added an example test

--- app/src/test/java/org/gnucash/android/asv/AccountTest.java
+++ app/src/test/java/org/gnucash/android/asv/AccountTest.java
@@ -1,4 +1,20 @@
 package org.gnucash.android.asv;
 
+import org.gnucash.android.model.Account;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class AccountTest {
+
+    @Test
+    public void exampleTest() {
+        Account testAccount = mock(Account.class);
+
+        when(testAccount.getName()).thenReturn("Klaas");
+
+        assertEquals(testAccount.getName(), "Klaas");
+    }
 }
