0fef115016f32149b790ad238c2f3ad1bac5d68f
Jordy Quak <jordy.quak@hva.nl>
2019-06-18 15:12:04 +0200

    Refactored an unneeded check and added a check if a split is not null

--- app/src/main/java/org/gnucash/android/db/adapter/TransactionsDbAdapter.java
+++ app/src/main/java/org/gnucash/android/db/adapter/TransactionsDbAdapter.java
@@ -119,7 +119,11 @@ public class TransactionsDbAdapter extends DatabaseAdapter<Transaction> {
                 } else {
                     mSplitsDbAdapter.addRecord(split, updateMethod);
                 }
-                splitUIDs.add(split.getUID());
+                if(split.getUID() == null) {
+                    Log.d(LOG_TAG, "Could not add split, specific split not found");
+                } else {
+                    splitUIDs.add(split.getUID());
+                }
             }
             Log.d(LOG_TAG, transaction.getSplits().size() + " splits added");
 
@@ -513,8 +517,6 @@ public class TransactionsDbAdapter extends DatabaseAdapter<Transaction> {
      * @return Number of splits belonging to the transaction
      */
     public long getSplitCount(@NonNull String transactionUID){
-        if (transactionUID == null)
-            return 0;
         String sql = "SELECT COUNT(*) FROM " + SplitEntry.TABLE_NAME
                 + " WHERE " + SplitEntry.COLUMN_TRANSACTION_UID + "= '" + transactionUID + "'";
         SQLiteStatement statement = mDb.compileStatement(sql);
